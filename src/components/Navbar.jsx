09e58bd2ce0c2041483a4234fd6817ba499950b7
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-08 12:36:46 +0100

    styled Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
   return (
    <div className={classes.navbar}>
       <h1>Jonah</h1>
-      <ul className="nav-menu">
+      <ul className={classes.navList}>
          <li>Home</li>
          <li>About me</li>
          <li>Services</li>
          <li>Portfolio</li>
          <li>Contact</li>
       </ul>
-      <div className="nav-connect">Connect With Me</div>
+      <div className={classes.navConnect}>Connect With Me</div>
    </div>
   )
 }
