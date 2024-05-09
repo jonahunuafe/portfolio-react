aa7ebf7e51ef1d2b6e0468526989d432a9e77344
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-09 13:37:50 +0100

    looping and displaying images of recent work

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./MyWork.module.css";
 import { mywork_data } from '../data';
+import { FaArrowRight } from "react-icons/fa"
 
 const MyWork = () => {
   return (
@@ -9,9 +10,13 @@ const MyWork = () => {
          <h1>My latest work</h1>
       </div>
       <div className={styles.myWorkContainer}>
-         {mywork_data.map((recentWork) => (
-            <img src={recentWork.w_img} alt="" />
+         {mywork_data.map((recentWork, index) => (
+            <img key={index} src={recentWork.w_img} alt="project images" />
          ))}
+      </div>
+      <div className={styles.showMore}>
+         <p>Show More</p>
+         <FaArrowRight />
       </div>
     </div>
   )
