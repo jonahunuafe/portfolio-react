ce72e212cb990601020f12f81e10f43d165bc28e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-09 15:12:24 +0100

    styled MyWork component

--- src/components/data.js
+++ src/components/data.js
@@ -1,8 +1,7 @@
-import designo_img from "./images/designo-img.jpg"
+import newslettter_img from "./images/newsletter_img.jpg"
 import e_commerce_img from "./images/e-commerce_img.webp"
 import manage_img from "./images/manage_img.webp";
-import payapi_img from "./images/payapi_img.webp";
-import tvshow_img from "./images/tvshow_img.jpg";
+import payapi_img from "./images/payapi_IMG.jpg";
 import dino_img from "./images/dino_img.png";
 
 
@@ -39,33 +38,28 @@ const services = [
 
 
 export const mywork_data = [
+   // {
+   //    w_no: 2,
+   //    w_name: "Payapi Multipage Website",
+   //    w_img: payapi_img
+   // },
+   // {
+   //    w_no: 3,
+   //    w_name: "Manage Landing Page",
+   //    w_img: manage_img
+   // },
    {
       w_no: 1,
       w_name: "Designo Multi-page Website",
-      w_img: designo_img
+      w_img: newslettter_img
    },
    {
-      w_no: 2,
-      w_name: "Payapi Multipage Website",
-      w_img: payapi_img
-   },
-   {
-      w_no: 3,
+      w_no: 4,
       w_name: "E-commerce Product Page",
       w_img: e_commerce_img
    },
    {
-      w_no: 4,
-      w_name: "Manage Landing Page",
-      w_img: manage_img
-   },
-   {
       w_no: 5,
-      w_name: "TV Show App",
-      w_img: tvshow_img
-   },
-   {
-      w_no: 6,
       w_name: "Dino Studio App",
       w_img: dino_img
    },
