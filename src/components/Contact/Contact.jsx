6d561f51c26495e0d4313126c3077ae9d15e8a7c
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-10 14:58:51 +0100

    implemented functionality to receive mails on Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Contact.module.css";
 import { IoLocationOutline } from "react-icons/io5";
 import { MdOutlineEmail } from "react-icons/md";
 import { IoCallOutline } from "react-icons/io5";
 
 const Contact = () => {
+   const [result, setResult] = useState("");
+
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "2155c00b-4466-4f0a-8578-2b154fb3ae49");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+   };
+  
   return (
       <div className={styles.contact}>
          <h1>Get in touch</h1>
@@ -28,13 +53,14 @@ const Contact = () => {
 
                </div>
             </div>
-            <form className={styles.contactRight}>
+            <form onSubmit={onSubmit} className={styles.contactRight}>
                <label htmlFor="">Your Name</label>
-               <input type="text" placeholder="Enter your name" name="name" />
+               <input type="text" placeholder="Enter your name" name="name" required />
                <label htmlFor="">Your Email</label>
-               <input type="email" placeholder="Enter your email" name="email" />
+               <input type="email" placeholder="Enter your email" name="email" required />
                <label htmlFor="">Write your message here</label>
                <textarea name="message" id="" rows="8" placeholder="Enter your message" />
+               <span>{result}</span>
                <button type="submit" className={styles.contactSubmit}>Submit</button>
             </form>
          </div>
