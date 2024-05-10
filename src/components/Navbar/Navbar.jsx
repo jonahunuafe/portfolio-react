a45a9a3737606675c07c693ac508d39b6cec1228
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-10 14:31:07 +0100

    refactor Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import { useState } from "react";
+import { NavLink } from 'react-router-dom';
 import { FaMinus } from "react-icons/fa";
 import classes from "./Navbar.module.css";
 
 
 const Navbar = () => {
-   const [menu, setMenu] = useState();
-   
+   const [menu, setMenu] = useState("/home");
+
   return (
    <div className={classes.navbar}>
       <h1>Jonah</h1>
       <ul className={classes.navList}>
-         <li><p onClick={() => setMenu("home")}>Home</p>{menu === "home" ? <FaMinus className={classes.minusSign} /> : <></>}</li>
-         <li><p onClick={() => setMenu("about")}>About me</p>{menu === "about" ? <FaMinus className={classes.minusSign} /> : <></>}</li>
-         <li><p onClick={() => setMenu("services")}>Services</p>{menu === "services" ? <FaMinus className={classes.minusSign} /> : <></>}</li>
-         <li><p onClick={() => setMenu("work")}>Portfolio</p>{menu === "work" ? <FaMinus className={classes.minusSign} /> : <></>}</li>
-         <li><p onClick={() => setMenu("contact")}>Contact</p>{menu === "contact" ? <FaMinus className={classes.minusSign} /> : <></>}</li>
+         <li onClick={() => setMenu("home")}>
+            <NavLink to="/">Home</NavLink>
+            {menu === "home" && <FaMinus className={classes.minusSign} />}
+         </li>
+         <li onClick={() => setMenu("about")}>
+            <NavLink to="/about">About me</NavLink>
+            {menu === "about" && <FaMinus className={classes.minusSign} />}
+            </li>
+         <li onClick={() => setMenu("services")}>
+            <NavLink to="/services">Services</NavLink>
+            {menu === "services" && <FaMinus className={classes.minusSign} />}
+            </li>
+         <li onClick={() => setMenu("work")}>
+            <NavLink to="/work">Portfolio</NavLink>
+            {menu === "work" && <FaMinus className={classes.minusSign} />}
+            </li>
+         <li onClick={() => setMenu("contact")}>
+            <NavLink to="/contact">Contact</NavLink>
+            {menu === "contact" && <FaMinus className={classes.minusSign} />}
+         </li>
       </ul>
       <div className={classes.navConnect}>Connect with me</div>
    </div>
