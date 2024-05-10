50c9621598bdf202d3bad5a17f145db4b83d2a0e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-10 13:38:31 +0100

    adding functionality to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
-// import React from 'react'
+import React from 'react'
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { FaMinus } from "react-icons/fa";
 import classes from "./Navbar.module.css";
 
 
 const Navbar = () => {
-   const [] = useState();
+   const [menu, setMenu] = useState();
 
 
   return (
    <div className={classes.navbar}>
       <h1>Jonah</h1>
       <ul className={classes.navList}>
-         <li>Home</li>
-         <li>About me</li>
-         <li>Services</li>
-         <li>Portfolio</li>
-         <li>Contact</li>
+         <li><p onClick={() => setMenu("home")}>Home</p>{menu === "home" ? <FaMinus /> : <></>}</li>
+         <li><p onClick={() => setMenu("about")}>About me</p>{menu === "about" ? <FaMinus /> : <></>}</li>
+         <li><p onClick={() => setMenu("services")}>Services</p>{menu === "services" ? <FaMinus /> : <></>}</li>
+         <li><p onClick={() => setMenu("work")}>Portfolio</p>{menu === "work" ? <FaMinus /> : <></>}</li>
+         <li><p onClick={() => setMenu("contact")}>Contact</p>{menu === "contact" ? <FaMinus /> : <></>}</li>
       </ul>
       <div className={classes.navConnect}>Connect with me</div>
    </div>
