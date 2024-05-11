c8fab08ac5e31abe4daae4b0f94086126d97e681
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-11 16:05:56 +0100

    refactor styling of Services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import styles from "./Services.module.css";
 import Service_Data from "../data";
-import { FaArrowRight } from "react-icons/fa"
 
 const Services = () => {
   return (
     <div className={styles.services}>
       <div className={styles.servicesTitle}>
          <h1>My Services</h1>
-         <img src="" alt="theme pattern" />
       </div>
       <div className={styles.servicesContainer}>
          {Service_Data.map((service, index) => (
             <div key={index} className={styles.servicesFormat}>
                <h3>{service.s_no}</h3>
                <h2>{service.s_name}</h2>
-               <FaArrowRight />
             </div>
          ))}
       </div>
