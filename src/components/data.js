4f4a638313bf44229a2514fc00ab7cdd8a0e9511
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-14 22:38:45 +0100

    imported images in data.js file

--- src/components/data.js
+++ src/components/data.js
@@ -1,8 +1,14 @@
 import newslettter_img from "./images/newsletter_img.jpg"
 import e_commerce_img from "./images/e-commerce_img.webp"
-import manage_img from "./images/manage_img.webp";
-import payapi_img from "./images/payapi_IMG.jpg";
+// import manage_img from "./images/manage_img.webp";
+// import payapi_img from "./images/payapi_IMG.jpg";
 import dino_img from "./images/dino_img.png";
+import elegant_img from "./images/elegant_img.png";
+import countdown_img from "./images/final countdown.png";
+import placepicker_img from "./images/placepicker.png";
+import projects_img from "./images/projects.png";
+import reactfundamentals_img from "./images/react-fundamentals.png";
+
 
 
 const services = [
@@ -44,17 +50,42 @@ export const mywork_data = [
    //    w_img: ""
    // },
    {
-      w_no: 2,
-      w_name: "Designo Multi-page Website",
+      w_no: 1,
+      w_name: "Newsletter page",
       w_img: newslettter_img
    },
    {
-      w_no: 3,
+      w_no: 2,
       w_name: "E-commerce Product Page",
       w_img: e_commerce_img
    },
    {
+      w_no: 3,
+      w_name: "Elegant Market",
+      w_img: elegant_img
+   },
+   {
       w_no: 4,
+      w_name: "Final Count Down App",
+      w_img: countdown_img
+   },
+   {
+      w_no: 5,
+      w_name: "Fundamentals of React",
+      w_img: reactfundamentals_img
+   },
+   {
+      w_no: 6,
+      w_name: "Plan Your Projects and Tasks",
+      w_img: projects_img
+   },
+   {
+      w_no: 7,
+      w_name: "Place Picker App",
+      w_img: placepicker_img
+   },
+   {
+      w_no: 8,
       w_name: "Dino Studio App",
       w_img: dino_img
    },
