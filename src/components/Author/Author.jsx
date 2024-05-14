ad2a6befbe13d2d160a66f890ca6085645172df0
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-14 10:37:06 +0100

    refactor Author.jsx component

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -8,14 +8,13 @@ import classes from "./Author.module.css";
 const Author = () => {
   return (
     <div className={classes.author}>
-      <img src={jonah_img} alt="author's profile picture" />
-      <h1><span>I'm Jonah Unuafe,</span> frontend developer based in Nigeria.</h1>
-      <p>I am a frontend developer from Delta State, South-South Nigeria with 5years experience in 
-        web development.
-      </p>
+      <div className={classes.AuthorImageContainer}><img src={jonah_img} alt="author's profile picture" /></div>
       <div className={classes.authorAction}>
+        <h1><span>Jonah Unuafe <br/></span> Frontend Developer.</h1>
+        <p>I am a frontend developer from Delta State, South-South Nigeria with 5years experience in 
+          web development.
+        </p>
         <div className={classes.authorConnect}><Link to="/contact">Connect with me</Link></div>
-        <div className={classes.authorResume}><Link to="/work">My resume</Link></div>
       </div>
     </div>
   );
