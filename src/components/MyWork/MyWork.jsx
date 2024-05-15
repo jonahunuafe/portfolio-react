365df1c7fa2e387440528802e58cebba2d776f85
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-15 15:04:45 +0100

    styled layer and added live url template to mywork component

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from "./MyWork.module.css";
 import { mywork_data } from '../data';
 import { FaArrowRight } from "react-icons/fa"
@@ -6,13 +7,19 @@ import { FaArrowRight } from "react-icons/fa"
 const MyWork = () => {
   return (
       <div className={styles.myWork}>
-         <h1>My latest work</h1>
+         <h1>Software Developer Portfolio</h1>
+         <p>
+            A collection of my featured projects. You can find more of my work on Github.
+         </p>
          <div className={styles.myWorkContainer}>
             {mywork_data.map((recentWork, index) => (
-               <div className={styles.imageContainer}>
-                  <img key={index} src={recentWork.w_img} alt="project images" />
-                  <div className={styles.layer}>
-                     <h3>{recentWork.w_name}</h3>  
+               <div>
+                  <h2>Hover to visit live url</h2>
+                  <div className={styles.imageContainer}>
+                     <img key={index} src={recentWork.w_img} alt="project images" />
+                     <div className={styles.layer}>
+                        <h3>{recentWork.w_name}</h3>
+                     </div>
                   </div>
                </div>
             ))}
