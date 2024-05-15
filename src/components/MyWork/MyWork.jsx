2c17b55b579e4b6d03bf74c82895acc8cfa20747
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-15 17:21:26 +0100

    import icon from font-awesome

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "./MyWork.module.css";
 import { mywork_data } from '../data';
-import { FaArrowRight } from "react-icons/fa"
+import { FaLink } from "react-icons/fa";
+
 
 const MyWork = () => {
   return (
       <div className={styles.myWork}>
-         <h1>Software Developer Portfolio</h1>
+         <h1>Frontend Developer Portfolio</h1>
          <p>
-            A collection of my featured projects. You can find more of my work on Github.
+            A collection of my featured projects. You can find more of my work on 
+            <Link to="https://github.com/jonahunuafe"> Github</Link>.
          </p>
          <div className={styles.myWorkContainer}>
             {mywork_data.map((recentWork, index) => (
                <div>
-                  <h4>Visit: <Link to={recentWork.live_url}>{recentWork.live_url}</Link></h4>
+                  <div className={styles.linkContainer}>
+                     <FaLink />
+                     <h4> 
+                        <Link to={recentWork.live_url}>{recentWork.live_url}</Link>
+                     </h4>
+                  </div>
                   <div className={styles.imageContainer}>
                      <img key={index} src={recentWork.w_img} alt="project images" />
                      <div className={styles.layer}>
@@ -24,12 +31,10 @@ const MyWork = () => {
                </div>
             ))}
          </div>
-         <div className={styles.showMore}>
-            <p>Show More</p>
-            <FaArrowRight />
-         </div>
       </div>
   );
 }
 
 export default MyWork; 
+
+// style={{color: "white"}}
