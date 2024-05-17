662647d6a86459451972b0adaf65e77a219f1238
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-17 13:41:32 +0100

    removed routing from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import React from 'react'
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Author from "./components/Author/Author"
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
@@ -9,21 +7,9 @@ import Footer from "./components/Footer/Footer"
 import RootLayout from './Layout/RootLayout';
 
 const App = () => {
-  // const router = createBrowserRouter([
-  //   {path: "/", element: <RootLayout />, children: [
-  //     {index: true, element: <Author />},
-  //     {path: "/about", element: <About />},
-  //     {path: "/services", element: <Services />},
-  //     {path: "/work", element: <MyWork />},
-  //     {path: "/contact", element: <Contact />},
-  //     {path: "/footer", element: <Footer />},
-  //   ]},
-  // ]);
-
   return (
     <div>
-      {/* <RouterProvider router={router}/> */}
-        <Author />
+      <Author />
     </div>
   )
 }
