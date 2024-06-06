ae34e998819b3c60dbfca17d71af6d4ffe692c4d
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-06 17:52:33 +0100

    getting started with resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import classes from "./Resume.module.css"
+import { Link } from 'react-router-dom';
 
 function MyResume() {
   return (
     <div>
-        MyResume
+        <header className={classes.header}>
+            <h1>Jonah Unuafe</h1>
+            <div className={classes.contactInfo}>
+                <span>+2348136250050</span>
+                <span>fejisfejiro@gmail.com</span>
+                <span>
+                    <Link to="https://www.linkedin.com/in/jonahunuafe">
+                        jonahunuafe
+                    </Link>
+                </span>
+                <span>
+                    <Link to="https://github.com/jonahunuafe">Github</Link>
+                </span>
+            </div>
+        </header>
+        <div className={classes.rightSide}>
+            <div>
+                <h1>Skills</h1>
+            </div>
+            <div>
+                <h1>Experience</h1>
+            </div>
+
+        </div>
+        <div className={classes.leftSide}>
+            <div>
+                <h1>Education</h1>
+            </div>
+            <div>
+                <h1></h1>
+            </div>
+        </div>
     </div>
   )
 }
