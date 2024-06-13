acf353e034557ef4ae31e3f808917fa92cb131e2
Jonah Unuafe <fejisfejiro@gmail.com>
2024-06-13 16:22:01 +0100

    added a download link to resume.js file

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -14,10 +14,11 @@ import { FaReact } from "react-icons/fa6";
 import { FaGitAlt } from "react-icons/fa";
 import { IoIosCloudOutline } from "react-icons/io";
 import { SiMongodb } from "react-icons/si";
+import Resume from "../../assets/images/tvshow_img.png";
 
 function MyResume() {
   return (
-    <>
+    <div id="resume">
         <header className={classes.header}>
             <h1>Jonah Unuafe</h1>
             <div className={classes.contactInfo}>
@@ -36,60 +37,61 @@ function MyResume() {
         </header>
         <div className={classes.resume}>
          
-                <div className={classes.skills}>
-                    <h1>Skills</h1>
-                    <div className={classes.expanded}>
-                        <h3>Frontend Development |</h3>
-                        <div className={classes.spanContainer}>
-                            <span><FaHtml5 /> HTML5</span>
-                            <span><IoLogoCss3 /> CSS3</span>
-                            <span><IoLogoJavascript /> JavaScript</span>
-                            <span><FaReact /> React</span>
-                            <span><FaGitAlt /> Git</span>
-                            <span><FaGithub /> Github</span>
-                            <span><IoIosCloudOutline /> Cloudinary</span>
-                            <span><SiMongodb /> MongoDB</span>
-                            <span><TbBrandNextjs /> Nextjs</span>
-                            <span>Bootstrap</span>
-                            <span>Tailwindcss</span>
-                        </div>
-                    </div>
-                </div>
-                <div className={classes.experience}>
-                    <h1>Work Experience</h1>
-                    <div className={classes.expanded}>
-                        <h3>Chingu |</h3>
-                        <p>
-                            Collaborated with other developers on chingu and created a 
-                            functional Dinosaur Educational app.
-                        </p>
-                    </div>
-                </div>
-                <div className={classes.projects}>
-                    <h1>Projects</h1>
-                    <p>
-                        Through Collaboration on Chingu, I built a Dino-Studio Educational App.
-                        I also built alot of projects on frontend mentor across various platforms.
-                        These projects and more, including my personal projects can be seen on my 
-                        <Link to="https://github.com/jonahunuafe"> Github page.</Link>
-                    </p>
-                </div>
-            
-            
-                <div className={classes.education}>
-                    <h1>Education</h1>
-                    <div className={classes.expanded}>
-                        <h3>Delta State University</h3>
-                        <span>B.sc Ed. (Chemistry Education)</span>
-                    </div>
-                    <div className={classes.expanded}>
-                        <h3>College of Education</h3>
-                        <span>National Certificate in Education (Physics/Chemistry)</span>
-                    </div>
+        <div className={classes.skills}>
+            <h1>Skills</h1>
+            <div className={classes.expanded}>
+                <h3>Frontend Development |</h3>
+                <div className={classes.spanContainer}>
+                    <span><FaHtml5 /> HTML5</span>
+                    <span><IoLogoCss3 /> CSS3</span>
+                    <span><IoLogoJavascript /> JavaScript</span>
+                    <span><FaReact /> React</span>
+                    <span><FaGitAlt /> Git</span>
+                    <span><FaGithub /> Github</span>
+                    <span><IoIosCloudOutline /> Cloudinary</span>
+                    <span><SiMongodb /> MongoDB</span>
+                    <span><TbBrandNextjs /> Nextjs</span>
+                    <span>Bootstrap</span>
+                    <span>Tailwindcss</span>
                 </div>
             </div>
+        </div>
+        <div className={classes.experience}>
+            <h1>Work Experience</h1>
+            <div className={classes.expanded}>
+                <h3>Chingu |</h3>
+                <p>
+                    Collaborated with other developers on chingu and created a 
+                    functional Dinosaur Educational app.
+                </p>
+            </div>
+        </div>
+        <div className={classes.projects}>
+            <h1>Projects</h1>
+            <p>
+                Through Collaboration on Chingu, I built a Dino-Studio Educational App.
+                I also built alot of projects on frontend mentor across various platforms.
+                These projects and more, including my personal projects can be seen on my 
+                <Link to="https://github.com/jonahunuafe"> Github page.</Link>
+            </p>
+        </div>
+    
+    
+        <div className={classes.education}>
+            <h1>Education</h1>
+            <div className={classes.expanded}>
+                <h3>Delta State University</h3>
+                <span>B.sc Ed. (Chemistry Education)</span>
+            </div>
+            <div className={classes.expanded}>
+                <h3>College of Education</h3>
+                <span>National Certificate in Education (Physics/Chemistry)</span>
+            </div>
+        </div>
         
-    </>
+        <a href="#resume" download={Resume}>Download Resume</a>
+    </div>    
+    </div>
   )
 }
 
