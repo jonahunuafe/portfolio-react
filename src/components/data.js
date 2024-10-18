ba48ef88a5b6026f48601164246ea8be5d169223
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-19 00:53:01 +0100

    updated projects page

--- src/components/data.js
+++ src/components/data.js
@@ -5,9 +5,9 @@ import payapi_img from "../assets/images/payapi_img.png";
 import designo_img from "../assets/images/designo_img.png";
 import tvshow_img from "../assets/images/tvshow_img.png";
 import dino_img from "../assets/images/dino_img.png";
-import newsroom_img from "../assets/images/newsroom_img.png"
-import carepulse_img from "../assets/images/carepulse-homepage.png"
-
+import newsroom_img from "../assets/images/newsroom_img.png";
+import carepulse_img from "../assets/images/carepulse-homepage.png";
+import jonahsBlog_img from "../assets/images/jonahsblog_img.png";
 
 const services = [
    { 
@@ -67,47 +67,53 @@ export const mywork_data = [
    },
    {
       w_no: 3,
-      live_url: "health-care-management-app.vercel.app",
+      live_url: "https://jonahsblog.netlify.app/",
+      w_techstack: "HTML, TAILWIND CSS, NEXT JS",
+      w_img: jonahsBlog_img, 
+      source_code: "https://github.com/jonahunuafe/blog-post"
+   },
+   {
+      w_no: 4,
+      live_url: "https://health-care-management-app.vercel.app",
       w_techstack: "HTML, TAILWIND CSS, NEXT JS, TYPESCRIPT",
       w_img: carepulse_img, 
       source_code: "https://github.com/jonahunuafe/health-care-management-app"
    },
    {
-      w_no: 4,
+      w_no: 5,
       live_url: "https://news-app-sage-six-29.vercel.app/",
       w_techstack: "HTML, TAILWIND CSS, NEXT JS, TYPESCRIPT",
       w_img: newsroom_img, 
       source_code: "https://github.com/jonahunuafe/news-app"
    },
    {
-      w_no: 5,
+      w_no: 6,
       live_url: "https://project-manager-lake-five.vercel.app",
       w_techstack: "HTML, CSS, REACT",
       w_img: projects_img,
       source_code: "https://github.com/jonahunuafe/project-manager"
    },
    {
-      w_no: 6,
+      w_no: 7,
       live_url: "https://place-picker-app.vercel.app",
       w_techstack: "HTML, CSS, REACT",
       w_img: placepicker_img, 
       source_code: "https://github.com/jonahunuafe/place-picker-app"
    },
    {
-      w_no: 7,
+      w_no: 8,
       live_url: "https://jonahunuafe.github.io/designo-multi-page-website/",
       w_techstack: "HTML, CSS, JAVASCRIPT",
       w_img: designo_img,
       source_code: "https://github.com/jonahunuafe/designo-multi-page-website",
    },
    {
-      w_no: 8,
+      w_no: 9,
       live_url: "https://jonahunuafe.github.io/payapi-multi-page-website/",
       w_techstack: "HTML, CSS, JAVASCRIPT",
       w_img: payapi_img,
       source_code: "https://github.com/jonahunuafe/payapi-multi-page-website"
    },
-   
 ]
 
 export default services;
